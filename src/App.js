bfe664e1704658f1c0d24df0529ec79a8779f31d
NatalliaPahosava <mulato4ka114@gmail.com>
2022-08-10 21:34:02 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import ColorComp from './components/Colors'
+import CounterComp from './components/Counter'
+import CompWithFragment from './components/WithFragment'
+import SuperSimpleComp from './components/Simple'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App-header' >
+      <ColorComp />
+      <CounterComp />
+      <CompWithFragment />
+      <SuperSimpleComp />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
